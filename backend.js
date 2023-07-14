5767680e845a9257b98230ce4e139e2c44a495ad
Livingstone <livingstone.lm.moyo@gmail.com>
2023-07-14 22:12:40 +0200

    add backed folder

---
+++ backend.js
@@ -0,0 +1,37 @@
+var inventory=[];
+var id=inventory.length+1
+       
+function addInventory(name,price){
+    
+    var product={id:id,item:name, price:price};
+    inventory.push(product); 
+    
+        
+}
+
+function refreshInventoryTable(){ 
+    i=0;
+    $.each(inventory,function(){        
+        $("#inventory-list").append( 
+            "<tr><td>"+inventory[i].id+"</td><td>"+inventory[i].item+"</td><td>"+inventory[i].price+"</td><td>"+0+"</td><td>"+0+"</td></tr>"     
+        );
+        i++;
+        $("td").addClass("w3-center");
+        $("td:first-of-type").addClass("w3-left")
+    })
+
+}
+
+function loadItemDropDown(){
+
+    i=0;
+    $.each(inventory,function(){        
+        $("#item").append( 
+            "<option>"+$('td').val()+"</option>"     
+        );
+        i++;
+ 
+    })
+
+}
+
--- vanilla.js
+++ vanilla.js
@@ -2,17 +2,54 @@
 
 $(document).ready(function(){ 
 
-    const a='{"id":2,"item":"Chelsea","price":21.00,"avail":6,"v":106}';
+
+    /*const a='{"id":2,"item":"Chelsea","price":21.00,"avail":6,"v":106}';
     y=JSON.parse(a);
     $("#inventory-list").append(
         "<tr><td>"+y.id+"</td><td>"+y.item+"</td><td>"+y.price+"</td><td>"+y.avail+"</td><td>"+y.v+"</td></tr>"    
     );
     $("td").addClass("w3-center");
     $("td:first-of-type").addClass("w3-left")
+        var o;
+
+    $.getJSON( "backend.json", function( data ) {
+        var i=0;
+        $.each(data.products,function(){
+            
+            $("#inventory-list").append(
+
+                "<tr><td>"+data.products[i].id+"</td><td>"+data.products[i].item+"</td><td>"+data.products[i].price+"</td><td>"+data.products[i].available+"</td><td>"+16+"</td></tr>"    
+          
+            );
+            i++;
+            $("td").addClass("w3-center");
+            $("td:first-of-type").addClass("w3-left")
+        })
+      });*/
+  
+
+      
 
     $("#show-add-new-item-pop-up").click(function()
         {
             $("#add-new-item-pop-up").fadeToggle();
+        });
+
+        $("#create-item-btn").click(function()
+        {           
+            $("#add-to-inventory-list-form").submit(function(event){
+                event.preventDefault()});
+
+                
+
+            $.getScript("backend.js",function(){
+                
+                addInventory($("#new-item-name").val(),$("#new-item-list-price").val());
+                refreshInventoryTable();
+                
+              })
+
+              
         });
 
     $("#close-add-new-item-pop-up").click(function()
@@ -21,6 +58,12 @@ $(document).ready(function(){
         });
 
     $(".show-transact-pop-up").click(function(){
+
+        /*$.getScript("backend.js",function(){      
+           loadItemDropDown();
+          })*/
+
+
      $("#transact-pop-up-title").text(($(this).text()));
      $("#transact-pop-up").fadeToggle();
     });
@@ -50,5 +93,7 @@ $(document).ready(function(){
    $("td:first-of-type").addClass("w3-left");
    })
    $("#grand-total").text(grandTotal);
+
+   
 
 });
