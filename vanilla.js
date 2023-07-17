bafd72270315eded8130011a610af3d0461e4f45
Livingstone <livingstone.lm.moyo@gmail.com>
2023-07-17 09:46:07 +0200

    Initial commit

---
+++ vanilla.js
@@ -0,0 +1,54 @@
+
+
+$(document).ready(function(){ 
+
+    const a='{"id":2,"item":"Chelsea","price":21.00,"avail":6,"v":106}';
+    y=JSON.parse(a);
+    $("#inventory-list").append(
+        "<tr><td>"+y.id+"</td><td>"+y.item+"</td><td>"+y.price+"</td><td>"+y.avail+"</td><td>"+y.v+"</td></tr>"    
+    );
+    $("td").addClass("w3-center");
+    $("td:first-of-type").addClass("w3-left")
+
+    $("#show-add-new-item-pop-up").click(function()
+        {
+            $("#add-new-item-pop-up").fadeToggle();
+        });
+
+    $("#close-add-new-item-pop-up").click(function()
+        {
+            $("#add-new-item-pop-up").fadeToggle();
+        });
+
+    $(".show-transact-pop-up").click(function(){
+     $("#transact-pop-up-title").text(($(this).text()));
+     $("#transact-pop-up").fadeToggle();
+    });
+
+   $("#close-transact-pop-up").click(function()
+   {
+    $("#transact-pop-up").fadeToggle();
+   });
+   var grandTotal=0;
+   $("#add-item-btn").click(function(){
+
+    $("#transact-form").submit(function(event){
+        event.preventDefault()});
+    
+       
+    var item =$("#item").val()
+    var price= 15;
+    var qty=Number($("#qty").val());
+    var total=price*qty;
+    grandTotal=grandTotal+total;
+    alert(grandTotal)
+
+    $("#items-list").append(
+        "<tr><td>"+item+"</td><td>"+price+"</td><td>"+qty+"</td><td>"+total+"</td></tr>"    
+    );
+    $("td").addClass("w3-center");
+   $("td:first-of-type").addClass("w3-left");
+   })
+   $("#grand-total").text(grandTotal);
+
+});
